3c1fe04ff38b2174d47e7da13de92b94cc25e0cc
Shaik Afnan <shaikafnan095@gmail.com>
2026-06-30 15:17:35 +0530

    add login view to views.py

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 
 @api_view(['POST'])
 def register(request):
@@ -12,18 +12,71 @@ def register(request):
         serializer = UserSerializer(data=request.data)
 
         if serializer.is_valid():
+            username = serializer.validated_data['username']
             password = serializer.validated_data['password']
-
-            hashed_password = make_password(password)
-
-            user = serializer.save(password=hashed_password)
-
+            
+            try:
+                user = Users.objects.get(
+                    username=username,
+                )
+            except Exception as e:
+                return Response({
+                    "success":False,
+                    "status":401,
+                    "message": "inavlid username or password"
+                }, status=status.HTTP_401_UNAUTHORIZED)
             return Response({
                 "success" : True,
                 "status" : 201,
-                "username" : user.username
+                "username" : user.username,
+                "message": "login successful"
             },status=status.HTTP_201_CREATED)
         
+        return Response({
+            "success" : False,
+            "status" : 403,
+            "message":"invalid request body"
+        }, status=status.HTTP_403_FORBIDDEN)
+    else:
+        return Response({
+            "success" : False,
+            "status" : 405,
+            "message" : f"invalid method required POST but got {request.method}"
+        }, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+@api_view(['POST'])
+def login(request):
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            username = serializer.validated_data['username']
+            password = serializer.validated_data['password']
+            
+            try:
+                user = Users.objects.get(
+                    username=username
+                )
+
+                if check_password(password, user.password):
+                    return Response({
+                        "success" : True,
+                        "status" : 201,
+                        "username" : user.username,
+                        "message" : "login successful"
+                    },status=status.HTTP_201_CREATED)
+                else:
+                    return Response({
+                        "success" : False,
+                        "status" : 401,
+                        "message" : "inavalid password"
+                    },status=status.HTTP_401_UNAUTHORIZED)
+            except Exception as _:
+                return Response({
+                        "success" : False,
+                        "status" : 401,
+                        "message" : "inavalid username or password"
+                    },status=status.HTTP_401_UNAUTHORIZED)
         return Response({
             "success" : False,
             "status" : 403

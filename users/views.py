47cf3e0ae829a79eae633553d977b3b7cbe1844f
Shaik Afnan <shaikafnan095@gmail.com>
2026-06-30 14:32:24 +0530

    add api view of post to register of user(password is hashed##)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Users
+from rest_framework import status
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
+@api_view(['POST'])
+def register(request):
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            password = serializer.cleaned_data['password']
+
+            hashed_password = make_password(password)
+
+            user = serializer.save(password=hashed_password)
+
+            return Response({
+                "status":"success"
+            },status=status.HTTP_201_CREATED)

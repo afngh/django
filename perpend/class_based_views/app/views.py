a57187a111b19b1810c726ed660a16044cd938bc
Shaik Afnan <shaikafnan095@gmail.com>
2026-07-03 13:38:00 +0530

    add class based views and csrf_exempt for testing

--- perpend/class_based_views/app/views.py
+++ perpend/class_based_views/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@method_decorator(csrf_exempt, name='dispatch')
+class MyView(View):
+    def get(self, request):
+        return HttpResponse('Hello, this is a GET request!')
+
+    def post(self, request):
+        return HttpResponse('Hello, this is a POST request!')
